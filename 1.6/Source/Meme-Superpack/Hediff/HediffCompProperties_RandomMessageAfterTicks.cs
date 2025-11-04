63e986f648207f29342263db79ce4d3b4b8166a1
RetroBoy99 <66008586+RetroBoy99@users.noreply.github.com>
2025-11-04 21:57:13 +0200

    add pregnent messages

---
+++ 1.6/Source/Meme-Superpack/Hediff/HediffCompProperties_RandomMessageAfterTicks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MSS.MemeSuperpack.Hediffs;
+
+public class HediffCompProperties_RandomMessageAfterTicks : HediffCompProperties
+{
+	public int ticks;
+	public LetterDef letterType;
+
+	[MustTranslate]
+	public List<string> letterTexts;
+
+	[MustTranslate]
+	public List<string> letterLabels;
+
+	public HediffCompProperties_RandomMessageAfterTicks()
+	{
+		compClass = typeof(HediffComp_RandomMessageAfterTicks);
+	}
+
+	public override IEnumerable<string> ConfigErrors(HediffDef parentDef)
+	{
+		if (ticks <= 0)
+			yield return "ticks must be a positive value";
+		if (letterTexts.NullOrEmpty())
+			yield return "message list is null or empty";
+		if (letterLabels.NullOrEmpty())
+			yield return "message list is null or empty";
+	}
+}
---
+++ 1.6/Source/Meme-Superpack/Hediff/HediffComp_RandomMessageAfterTicks.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using RimWorld;
+using Verse;
+
+namespace MSS.MemeSuperpack.Hediffs;
+
+public class HediffComp_RandomMessageAfterTicks : HediffComp
+{
+	private int ticksUntilMessage;
+
+	protected HediffCompProperties_RandomMessageAfterTicks Props
+	{
+		get => (HediffCompProperties_RandomMessageAfterTicks)props;
+	}
+
+	public override void CompPostMake()
+	{
+		base.CompPostMake();
+		ticksUntilMessage = Props.ticks;
+	}
+
+	public override void CompPostTick(ref float severityAdjustment)
+	{
+		base.CompPostTick(ref severityAdjustment);
+
+		if (ticksUntilMessage == 0)
+		{
+			if (PawnUtility.ShouldSendNotificationAbout(Pawn))
+			{
+				if (Props.letterType != null)
+					Find.LetterStack.ReceiveLetter(
+						Props.letterLabels.RandomElement().Formatted((NamedArgument)(Thing)Pawn),
+						GetLetterText(),
+						Props.letterType,
+						(Thing)Pawn
+					);
+			}
+		}
+		else
+		{
+			if (ticksUntilMessage <= 0)
+				return;
+		}
+
+		--ticksUntilMessage;
+	}
+
+	public override void CompExposeData()
+	{
+		base.CompExposeData();
+		Scribe_Values.Look(ref ticksUntilMessage, "ticksUntilMessage");
+	}
+
+	private TaggedString GetLetterText()
+	{
+		StringBuilder text = new StringBuilder(
+			Props.letterTexts.RandomElement().Formatted((NamedArgument)(Thing)Pawn)
+		);
+
+		if (parent is not Hediff_Pregnant p || p.Mother == null || p.Mother == p.pawn)
+			return text.ToString();
+		text.AppendLine("IvfPregnancyLetterText".Translate(parent.pawn.NameFullColored));
+
+		if (p.Mother != null && p.Father != null)
+			text.AppendLine("");
+		text.AppendLine(
+			"IvfPregnancyLetterParents".Translate(p.Mother.NameFullColored, p.Father.NameFullColored)
+		);
+		return text.ToString();
+	}
+}
